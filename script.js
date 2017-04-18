52fe010a79a1662612187a7671783f1d100083e9
h3xar0n <aron@glambyadelina.com>
2017-04-18 15:24:02 -0400

    Error message for missing email and password

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ var main = function() {
   });
   $('form').submit(function () {
     var email = $('#email').val();
-    
+    var password = $('#password').val();
+
     if(email == "") {
-      $('.email-error').text("Please enter your password.");
+      $('.email-error').text("Please enter your email.");
+    };
+
+    if(password == "") {
+      $('.password-error').text("Please enter your password.");
     };
 
     return false;
